7a667ef202f6ed6d9b6740f81758d9afae6b08c7
heesungjang <heesungj7@gmail.com>
2022-01-23 21:34:10 +1100

    set in swift

--- 31-working-with-sets/begin/Part 4 - Episode 5 - Working with Sets - Begin.playground/Contents.swift
+++ 31-working-with-sets/begin/Part 4 - Episode 5 - Working with Sets - Begin.playground/Contents.swift
@@ -1,2 +1,39 @@
 //: ## Episode 05: Creating and Populating Sets
 
+
+
+var array = ["heesung", "zion"]
+
+array
+
+
+array.append("mimi")
+
+array
+
+
+
+var set: Set<Int> = [1, 2]
+
+set.remove(1)
+
+var nameSet: Set<String> = ["heesung", "zion"]
+
+let popName = nameSet.remove("heesung")
+
+if let popName = popName {
+  print(popName)
+}
+
+var anotherSet: Set<String> = ["zion"]
+
+var intersection = nameSet.intersection(anotherSet)
+
+let poped = intersection.remove("zion")
+
+
+if let poped = poped {print(poped)}
+
+
+
+
